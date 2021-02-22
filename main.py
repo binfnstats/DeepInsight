ee5bfb2026fa3e4d47ae1540c16ff64006fb0571
Ted Wong <tedwong@smallchess.com>
2021-02-22 14:24:29 +1100

    ...

---
+++ main.py
@@ -0,0 +1,14 @@
+#
+# python3 convert.py <Gene Matrix TSV Format> <Labels> <Output>
+#
+# DeepInsight image conversion. Only RNA is supported in this release.
+#
+
+import os
+import sys
+
+def convert(data, lab, out):
+
+
+
+convert(sys.argv[1], sys.argv[2], sys.argv[3])
